bc921ccefaba0cb19684247022624775a44d2128
Victor-Aremu <victoraremu123@gmail.com>
2021-11-11 11:51:06 +0000

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Routes/Home/Home";
+import Login from "./Routes/Login/Login";
+import SignUp from "./Routes/SignUp/SignUp";
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
