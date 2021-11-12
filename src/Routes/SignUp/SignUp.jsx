6a74b0aaf9338861dfb679f64b62841fa113ebb9
Victor-Aremu <victoraremu123@gmail.com>
2021-11-12 16:05:17 +0000

    adding signup

--- src/Routes/SignUp/SignUp.jsx
+++ src/Routes/SignUp/SignUp.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Footer from "../../Components/Footer/Footer";
+import Navbar from "../../Components/Navbar/Navbar";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return <div className="signUp">
+    <Navbar/>
+    <Footer/>
+  </div>;
 };
 
 export default SignUp;
