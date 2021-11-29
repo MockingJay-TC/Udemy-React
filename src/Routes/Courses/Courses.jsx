38036c050f354a2b21fce3c9bddbb70a5f6a1693
Victor-Aremu <victoraremu123@gmail.com>
2021-11-29 11:25:50 +0000

    fetching data from api

--- src/Routes/Courses/Courses.jsx
+++ src/Routes/Courses/Courses.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Course from "./Course";
 import "./Course.css";
+import Axios from "axios";
 
 const Courses = () => {
+  const [courses, setCourses] = useState([]);
+  console.log(courses);
+  useEffect(() => {
+    const url = "http://localhost:1337/courses";
+    Axios.get(url).then((res) => {
+      setCourses(res.data);
+    });
+  }, []);
+
   return (
     <div className="row mx-5 my-3 my__course">
-      <Course />
-      <Course />
-      <Course />
-      <Course />
-      <Course />
-      <Course />
-      <Course />
-      <Course />
-      <Course />
-      <Course />
+      {courses.map((course) => {
+        return (
+          <Course
+            key={course.id}
+            name={course.name}
+            description={course.description}
+            instructors={course.instructors}
+          />
+        );
+      })}
     </div>
   );
 };
